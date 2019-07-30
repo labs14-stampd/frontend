1b5758b919a85eb0352bbc14183dc278a404fff7
cecil john tantay <cjbtantay@gmail.com>
2019-07-30 08:54:17 -0700

    Deletes old client and truffle foldders

--- bs-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  port: process.env.PORT,
-  files: ['./client/src/**/*.{html,htm,css,js}'],
-  server:{
-    baseDir: [".client/src", "./build/contracts"]
-  }};
--- client/src/auth_config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  domain: process.env.REACT_APP_AUTH0_DOMAIN,
-  clientId: process.env.REACT_APP_AUTH0_CLIENT_ID
-}
