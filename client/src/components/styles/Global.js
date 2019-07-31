cb268dad941f1a6fe9f68192de1f12cbdf464737
BrannanC <brannanconrad@gmail.com>
2019-07-31 15:55:12 -0500

    Style framework

---
+++ client/src/components/styles/Global.js
@@ -0,0 +1,60 @@
+import { css } from "styled-components";
+import { theme } from "./themes";
+
+export const Global = css`
+  * {
+    box-sizing: border-box;
+  }
+  html {
+    font-size: 62.5%;
+    scroll-behavior: smooth;
+  }
+  h1,
+  h2,
+  h3,
+  h4 {
+    font-family: "Work Sans", sans-serif;
+  }
+  h1 {
+    font-size: 3.8rem;
+  }
+  h4 {
+    font-size: 1.6rem;
+    font-weight: 200;
+    opacity: 0.9;
+    letter-spacing: 4px;
+    text-transform: uppercase;
+  }
+  p {
+    font-family: "Work Sans", sans-serif;
+    font-size: 3rem;
+    line-height: 1.8;
+  }
+  a {
+    text-decoration: none;
+    font-size: 2rem;
+    font-family: "Work Sans", sans-serif;
+    color: black;
+  }
+  input,
+  textarea,
+  button {
+    -webkit-appearance: none;
+    font-family: "Work Sans", sans-serif;
+  }
+  label {
+    font-size: 1.6rem;
+    font-weight: 200;
+    letter-spacing: 4px;
+    text-transform: uppercase;
+    font-family: "Work Sans", sans-serif;
+  }
+  input,
+  textarea {
+    color: #929292;
+  }
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
---
+++ client/src/components/styles/index.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+import { Reset } from "./Reset";
+import { Global } from "./Global";
+
+export const GlobalStyle = createGlobalStyle`
+  ${Reset}
+  ${Global}
+  background: red;
+`;
