97bf1c51ca917d1f75120bd98bef58a6b04fa95c
joepound <14357797+joepound@users.noreply.github.com>
2019-08-01 09:53:53 -0600

    Added private routes for dashboard and onboarding form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './auth/PrivateRoute.jsx';
 
 import './App.css';
 
 import { Layout } from './components/Layout';
 import { Dashboard } from './components/dashboard';
+import { OnboardForm } from './components/dashboard/onboarding';
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <Dashboard />
+        <Switch>
+          <Route exact path="/" /> {/* Placeholder route - to be used later */}
+          <PrivateRoute path="/onboarding" render={OnboardForm}  />
+          <PrivateRoute path="/dashboard" render={Dashboard}  />
+        </Switch>
       </Layout>
     </div>
   );
