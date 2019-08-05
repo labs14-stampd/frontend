00cb4fc517ea009b1af2076a9978d3b7d8e0ce71
joepound <14357797+joepound@users.noreply.github.com>
2019-08-05 13:57:39 -0600

    Add Field styled component

---
+++ client/src/components/Field.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const Field = ({
+  labelText,
+  inputName,
+  placeholder,
+  onChange,
+  inputValue,
+  required = false
+}) => {
+  const Label = styled.label`
+    font-weight: bold;
+    color: red;
+
+    ${required &&
+      `
+    &::after {
+      content: "*";
+      color: red;
+      margin-left: 5px;
+    }
+    `}
+  `;
+  const Input = styled.input`
+    background-color: pink;
+  `;
+
+  return (
+    <>
+      <Label>{labelText}</Label>
+      <Input
+        name={inputName}
+        placeholder={placeholder}
+        onChange={onChange}
+        value={inputValue}
+        required={required}
+      />
+    </>
+  );
+};
+
+export default Field;
