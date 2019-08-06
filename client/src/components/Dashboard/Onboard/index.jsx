62583310fb8053a641763cbf0633eeb205f0d648
cecil john tantay <cjbtantay@gmail.com>
2019-08-06 10:45:47 -0700

    adds more linting

--- client/src/components/Dashboard/Onboard/index.jsx
+++ client/src/components/Dashboard/Onboard/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Onboard({ history }) {
+  console.log(history);
   return (
     <>
-      <button onClick={() => history.push('/onboarding/school')}>I am a School</button>
+      <button type="button" onClick={() => history.push('/onboarding/school')}>
+        I am a School
+      </button>
     </>
   );
 }
+
+Onboard.propTypes = {
+  history: PropTypes.object
+};
 
 export default Onboard;
