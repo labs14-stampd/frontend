b37491e8fcd5a41f01e6187156a03d83a91d0631
Nathan Thomas <nwthomas@me.com>
2019-08-06 09:03:49 -0700

    Update Jest configuration

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,16 +1,25 @@
 module.exports = {
+  // All imported modules in your tests should be mocked automatically
+  automock: true,
+
+  // Stop running tests after `n` failures
+  bail: 1,
+
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
 
-  // Displays the name of the repository beside test suites while running tests
+  // Displays the current testing suite name beside each test as it runs
   displayName: 'Stampd Client',
 
-  // The test environment that will be used for testing
-  testEnvironment: 'testing',
+  // An array of file extensions your modules use
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
 
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['/node_modules/'],
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: ['/node_modules/'],
 
   // Indicates whether each individual test should be reported during the run
-  verbose: true
+  verbose: true,
+
+  // Whether to use watchman for file crawling
+  watchman: true
 };
