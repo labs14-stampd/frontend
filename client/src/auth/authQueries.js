323a1f6aefcc61821e88f516cf7ed034d29f35c8
joepound <14357797+joepound@users.noreply.github.com>
2019-08-06 12:18:45 -0600

    Set auth queries file to have a default export

--- client/src/auth/authQueries.js
+++ client/src/auth/authQueries.js
@@ -1,19 +1,23 @@
 import client, { gql } from '../clientQuery';
 
-export const register = user => {
-  return client.mutate({
-    variables: {
-      authToken: user.authToken
-    },
-    mutation: gql`
-      mutation AddUser($authToken: String!) {
-        addUser(authToken: $authToken) {
-          id
-          username
-          email
-          roleId
+const queries = {
+  register(user) {
+    return client.mutate({
+      variables: {
+        authToken: user.authToken
+      },
+      mutation: gql`
+        mutation AddUser($authToken: String!) {
+          addUser(authToken: $authToken) {
+            id
+            username
+            email
+            roleId
+          }
         }
-      }
-    `
-  });
-};
+      `
+    });
+  }
+}
+
+export default queries;
