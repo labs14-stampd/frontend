38c55b400ea7e324ffce3154ee0456e69db4bdad
Nathan Thomas <nwthomas@me.com>
2019-08-07 09:13:33 -0700

    Remove babel-loader and setup App.test.js test

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+import { renderWithRouter, rendererWithRouter } from './testHelpers';
+
+import App from './App';
+
+describe('<App />', () => {
+  it('should render the App component without crashing', () => {
+    const helpers = renderWithRouter(
+      <App history={{ location: { pathname: '/' } }} />
+    );
+  });
+
+  it('matches the snapshot of App', () => {
+    const tree = rendererWithRouter(
+      <App history={{ location: { pathname: '/' } }} />
+    );
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
