a8ac8783e19d804f173a8ec4dc76cc37f5bb9b34
cecil john tantay <cjbtantay@gmail.com>
2019-08-08 11:11:17 -0700

    add heading and button styles for form

--- client/src/components/Dashboard/Onboard/SchoolDetailsForm.jsx
+++ client/src/components/Dashboard/Onboard/SchoolDetailsForm.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { MaskedInput } from 'grommet';
+import { MaskedInput, Select, Box, Heading } from 'grommet';
 
-import { BaseForm, BaseTextInput, BaseFormField } from '../../../styles/themes';
+import {
+  BaseForm,
+  BaseTextInput,
+  BaseFormField,
+  BaseButton
+} from '../../../styles/themes';
 import queries from './queries';
 
 const SchoolDetailsForm = ({ history }) => {
@@ -20,6 +25,68 @@ const SchoolDetailsForm = ({ history }) => {
     url: '',
     userId: localStorage.id
   });
+
+  const states = [
+    'AL',
+    'AK',
+    'AS',
+    'AZ',
+    'AR',
+    'CA',
+    'CO',
+    'CT',
+    'DE',
+    'DC',
+    'FM',
+    'FL',
+    'GA',
+    'GU',
+    'HI',
+    'ID',
+    'IL',
+    'IN',
+    'IA',
+    'KS',
+    'KY',
+    'LA',
+    'ME',
+    'MH',
+    'MD',
+    'MA',
+    'MI',
+    'MN',
+    'MS',
+    'MO',
+    'MT',
+    'NE',
+    'NV',
+    'NH',
+    'NJ',
+    'NM',
+    'NY',
+    'NC',
+    'ND',
+    'MP',
+    'OH',
+    'OK',
+    'OR',
+    'PW',
+    'PA',
+    'PR',
+    'RI',
+    'SC',
+    'SD',
+    'TN',
+    'TX',
+    'UT',
+    'VT',
+    'VI',
+    'VA',
+    'WA',
+    'WV',
+    'WI',
+    'WY'
+  ];
 
   const handleChanges = e => {
     setInput({
@@ -42,117 +109,122 @@ const SchoolDetailsForm = ({ history }) => {
 
     history.push('/dashboard');
   };
+
   return (
     <SchoolForm onSubmit={handleSubmit}>
-      <SchoolFormField label="Institution">
-        <SchoolBaseTextInput
-          name="name"
-          placeholder="Lambda School"
-          onChange={handleChanges}
-          value={input.name}
-          plain={false}
-          required
-        />
-      </SchoolFormField>
-      <SchoolFormField label="Tax Id">
-        <SchoolBaseTextInput
-          labelText="TaxId"
-          name="taxId"
-          placeholder="TaxId"
-          onChange={handleChanges}
-          value={input.taxId}
-          required
-        />
-      </SchoolFormField>
-      <SchoolFormField label="Address 1">
-        <SchoolBaseTextInput
-          name="street1"
-          placeholder="Address 1"
-          onChange={handleChanges}
-          value={input.street1}
-        />
-      </SchoolFormField>
-      <SchoolFormField label="Address 2">
-        <SchoolBaseTextInput
-          name="street2"
-          placeholder="Address 2"
-          onChange={handleChanges}
-          value={input.street2}
-        />
-      </SchoolFormField>
-      <SchoolFormField label="City">
-        <SchoolBaseTextInput
-          name="city"
-          placeholder="City"
-          onChange={handleChanges}
-          value={input.city}
-        />
-      </SchoolFormField>
-      <SchoolFormField label="State">
-        <SchoolBaseTextInput
+    <Box direction="column" animation="jiggle">
+      <Heading margin="20px 0 0 0" alignSelf="center">School Details</Heading>
+        <SchoolFormField label="Institution">
+          <SchoolBaseTextInput
+            name="name"
+            placeholder="Lambda School"
+            onChange={handleChanges}
+            value={input.name}
+            plain={false}
+            required
+          />
+        </SchoolFormField>
+        <SchoolFormField label="Tax Id">
+          <SchoolBaseTextInput
+            labelText="TaxId"
+            name="taxId"
+            placeholder="TaxId"
+            onChange={handleChanges}
+            value={input.taxId}
+            required
+          />
+        </SchoolFormField>
+        <SchoolFormField label="Address 1">
+          <SchoolBaseTextInput
+            name="street1"
+            placeholder="123 Fake street"
+            onChange={handleChanges}
+            value={input.street1}
+          />
+        </SchoolFormField>
+        <SchoolFormField label="Address 2">
+          <SchoolBaseTextInput
+            name="street2"
+            placeholder="Apt B"
+            onChange={handleChanges}
+            value={input.street2}
+          />
+        </SchoolFormField>
+        <SchoolFormField label="City">
+          <SchoolBaseTextInput
+            name="city"
+            placeholder="San Francisco"
+            onChange={handleChanges}
+            value={input.city}
+          />
+        </SchoolFormField>
+        <SchoolFormField
+          label="State"
           name="state"
-          placeholder="State"
-          onChange={handleChanges}
+          component={Select}
+          options={states}
+          onChange={({ option }) => setInput({ ...input, state: option })}
           value={input.state}
+          placeholder="State"
         />
-      </SchoolFormField>
-      <SchoolFormField label="Zip Code">
-        <SchoolBaseTextInput
-          name="zip"
-          placeholder="Zip Code"
-          onChange={handleChanges}
-          value={input.zip}
-        />
-      </SchoolFormField>
-      <SchoolFormField label="Phone Number">
-        <SchoolMaskedInput
-          mask={[
-            { fixed: '(' },
-            {
-              length: 3,
-              regexp: /^[0-9]{1,3}$/,
-              placeholder: 'xxx'
-            },
-            { fixed: ')' },
-            { fixed: ' ' },
-            {
-              length: 3,
-              regexp: /^[0-9]{1,3}$/,
-              placeholder: 'xxx'
-            },
-            { fixed: '-' },
-            {
-              length: 4,
-              regexp: /^[0-9]{1,4}$/,
-              placeholder: 'xxxx'
-            }
-          ]}
-          value={input.phone}
-          name="phone"
-          onChange={handleChanges}
-          required
-        />
-      </SchoolFormField>
-      <SchoolFormField label="Type of Institution">
-        <SchoolBaseTextInput
-          name="type"
-          placeholder="Type of Institution"
-          onChange={handleChanges}
-          value={input.type}
-        />
-      </SchoolFormField>
-      <SchoolFormField label="Institution Website">
-        <SchoolBaseTextInput
-          labelText="Institution Website"
-          name="url"
-          placeholder="Institution Website"
-          onChange={handleChanges}
-          value={input.url}
-          required
-        />
-      </SchoolFormField>
-      <button type="submit">Submit</button>
-    </SchoolForm>
+        <SchoolFormField label="Zip Code">
+          <SchoolBaseTextInput
+            name="zip"
+            placeholder="90210"
+            onChange={handleChanges}
+            value={input.zip}
+          />
+        </SchoolFormField>
+        <SchoolFormField label="Phone Number">
+          <SchoolMaskedInput
+            mask={[
+              { fixed: '(' },
+              {
+                length: 3,
+                regexp: /^[0-9]{1,3}$/,
+                placeholder: 'xxx'
+              },
+              { fixed: ')' },
+              { fixed: ' ' },
+              {
+                length: 3,
+                regexp: /^[0-9]{1,3}$/,
+                placeholder: 'xxx'
+              },
+              { fixed: '-' },
+              {
+                length: 4,
+                regexp: /^[0-9]{1,4}$/,
+                placeholder: 'xxxx'
+              }
+            ]}
+            value={input.phone}
+            name="phone"
+            onChange={handleChanges}
+            required
+          />
+        </SchoolFormField>
+        <SchoolFormField label="Type of Institution">
+          <SchoolBaseTextInput
+            name="type"
+            placeholder="University"
+            onChange={handleChanges}
+            value={input.type}
+          />
+        </SchoolFormField>
+        <SchoolFormField label="Institution Website">
+          <SchoolBaseTextInput
+            labelText="Institution Website"
+            name="url"
+            placeholder="ls.dev"
+            onChange={handleChanges}
+            value={input.url}
+            required
+          />
+        </SchoolFormField>
+        <SchoolButton type="submit" primary label="Submit" alignSelf="center" />
+    </Box>
+      </SchoolForm>
   );
 };
 
@@ -161,12 +233,17 @@ SchoolDetailsForm.propTypes = {
 };
 
 const SchoolForm = styled(BaseForm)`
-  margin: 120px auto 50px;
+  margin: 120px auto 100px;
   background-color: white;
   border: 1px solid #d8d8d8;
   border-radius: 2px;
   max-width: 800px;
   width: 100%;
+`;
+
+const SchoolButton = styled(BaseButton)`
+  text-align: center;
+  margin: 15px 20px 50px;
 `;
 
 const SchoolBaseTextInput = styled(BaseTextInput)`
