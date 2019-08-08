51db8a04155bcc6741e812ad599d3747e89055c1
cecil john tantay <cjbtantay@gmail.com>
2019-08-07 18:43:03 -0700

    change dashboard back to privatRoute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import { Grommet } from 'grommet';
 
 import PrivateRoute from './auth/PrivateRoute';
-import {useAuth0} from './auth/authWrapper';
+import { useAuth0 } from './auth/authWrapper';
 import { theme } from './styles/themes';
 import Loading from './components/Layout/Loading';
 import Dashboard from './components/Dashboard';
@@ -19,28 +19,28 @@ function App() {
   const { loading } = useAuth0();
   return (
     <>
-      {loading ? <Loading /> : (
-        
+      {loading ? (
+        <Loading />
+      ) : (
         <Grommet theme={theme}>
-        <Route exact path="/" component={Landing} />
-        <Layout>
-          <Switch>
-            <PrivateRoute
-              path="/onboarding/school"
-              component={SchoolDetailsForm}
-            />
-            <PrivateRoute exact path="/onboarding" component={Onboard} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <PrivateRoute
-              path="/dashboard/credForm"
-              component={CredentialForm}
-            />
-          </Switch>
-        </Layout>
-        <GlobalStyle />
-      </Grommet>
+          <Route exact path="/" component={Landing} />
+          <Layout>
+            <Switch>
+              <PrivateRoute
+                path="/onboarding/school"
+                component={SchoolDetailsForm}
+              />
+              <PrivateRoute exact path="/onboarding" component={Onboard} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                path="/dashboard/credForm"
+                component={CredentialForm}
+              />
+            </Switch>
+          </Layout>
+          <GlobalStyle />
+        </Grommet>
       )}
-
     </>
   );
 }
