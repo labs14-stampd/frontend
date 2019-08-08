23b788879797633ca3206a5f517748294a7a6b44
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-08-08 15:34:45 -0500

    attempting to fix the footer

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -11,12 +11,15 @@ const Footer = () => {
 
 const FooterContainer = styled.div`
   text-align: center;
-  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
   bottom: 0;
   width: 100%;
   color: #333333;
-  padding: 50px 0 10px;
   z-index: 5;
+  height: 40px;
 
   p {
     font-size: 1.4rem;
--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Grommet } from 'grommet';
 import PropTypes from 'prop-types';
 import NavBar from './NavBar.jsx';
-import Footer from './Footer.jsx'
+import Footer from './Footer.jsx';
 import { theme } from '../../styles/themes';
+import styled from 'styled-components';
 
 // GlobalStyles will go here
 
@@ -11,9 +12,11 @@ const Layout = ({ children }) => {
   return (
     <>
       <Grommet theme={theme}>
-        <NavBar />
-        {children}
-        <Footer />
+        <Container>
+          <NavBar />
+          {children}
+          <Footer />
+        </Container>
       </Grommet>
     </>
   );
@@ -25,5 +28,11 @@ Layout.propTypes = {
     PropTypes.node
   ]).isRequired
 };
+
+const Container = styled.main`
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
+`;
 
 export default Layout;
