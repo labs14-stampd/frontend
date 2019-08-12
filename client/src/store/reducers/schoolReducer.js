d1a9b7da1c3093841ccc2ac08cc2763c4bf7be29
cecil john tantay <cjbtantay@gmail.com>
2019-08-12 10:44:13 -0700

    add fuzzy search and remove console log in reducer

--- client/src/store/reducers/schoolReducer.js
+++ client/src/store/reducers/schoolReducer.js
@@ -3,6 +3,7 @@ import { createContext } from 'react';
 export const SCHOOL_DATA_START = 'SCHOOL_DATA_START';
 export const SCHOOL_DATA_SUCCESS = 'SCHOOL_DATA_SUCCESS';
 export const SCHOOL_DATA_ERROR = 'SCHOOL_DATA_ERROR';
+export const SEARCH_HANDLE_CHANGE = 'SEARCH_HANDLE_CHANGE';
 
 export const schoolContext = createContext();
 
@@ -10,11 +11,11 @@ const initialState = {
   schoolData: null,
   schoolDataSuccess: false,
   schoolDataStart: false,
-  schoolDataError: false
+  schoolDataError: false,
+  schoolSearchInput: ''
 };
 
 export const schoolReducer = (state = initialState, action) => {
-  console.log(action);
   switch (action.type) {
     case SCHOOL_DATA_START:
       return {
@@ -36,6 +37,11 @@ export const schoolReducer = (state = initialState, action) => {
         ...state,
         schoolDataError: true,
         schoolDataStart: false
+      };
+    case SEARCH_HANDLE_CHANGE:
+      return {
+        ...state,
+        schoolSearchInput: action.payload
       };
     default:
       return state;
