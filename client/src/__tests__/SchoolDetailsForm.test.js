b0f95e3f281c23a16ddf5daad0c7e3b2e36ccb90
Nathan Thomas <nwthomas@me.com>
2019-08-19 16:16:00 -0700

    Update with additional tests for the SchoolDetailsForm

--- client/src/__tests__/SchoolDetailsForm.test.js
+++ client/src/__tests__/SchoolDetailsForm.test.js
@@ -9,9 +9,43 @@ import {
 import SchoolDetailsForm from '../components/Dashboard/Onboard/SchoolDetailsForm';
 
 describe('<SchoolDetailsForm />', () => {
-  it('should render the component successfully without crashing', () => {
-    const helpers = renderWithRouterAndProviders(
-      <SchoolDetailsForm history={{ location: { pathname: '/' } }} />
-    );
+  describe('should render the component successfully without crashing', () => {
+    it('tests that the component does not crash when it is rendered', () => {
+      const helpers = renderWithRouterAndProviders(
+        <SchoolDetailsForm history={{ location: { pathname: '/' } }} />
+      );
+    });
+  });
+
+  describe('should match the previous snapshot of the component', () => {
+    it('tests that the component has not changed and matched the previous snapshot', () => {
+      const tree = rendererWithRouterAndProviders(
+        <SchoolDetailsForm history={{ location: { pathname: '/' } }} />
+      );
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+  });
+
+  describe('should render headers and labels to the screen successfully', () => {
+    it('tests that the header test renders correctly', () => {
+      const { getByText } = renderWithRouterAndProviders(
+        <SchoolDetailsForm history={{ location: { pathname: '/' } }} />
+      );
+      expect(getByText(/School Register/i)).toBeDefined();
+    });
+
+    it('tests the Institution label renders correctly', () => {
+      const { getByText } = renderWithRouterAndProviders(
+        <SchoolDetailsForm history={{ location: { pathname: '/' } }} />
+      );
+      expect(getByText(/Institution/i)).toBeDefined();
+    });
+
+    it('tests the Institution label renders correctly', () => {
+      const { getAllByText } = renderWithRouterAndProviders(
+        <SchoolDetailsForm history={{ location: { pathname: '/' } }} />
+      );
+      expect(getAllByText(/Institution/i)).toBeDefined();
+    });
   });
 });
