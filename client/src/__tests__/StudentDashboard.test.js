8b95bfc740a4057e70a3678bb1560b3ba95fce9a
Nathan Thomas <nwthomas@me.com>
2019-08-21 08:20:26 -0700

    Craft additional mock tests for StudentDashboard.test.js

---
+++ client/src/__tests__/StudentDashboard.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import {
+  renderWithRouterAndProviders,
+  rendererWithRouterAndProviders
+} from '../__testHelpers__';
+
+import StudentDashboard from '../components/Dashboard/Dashboard/StudentDashboard';
+
+describe('<StudentDashboard />', () => {
+  describe('should render the component successfully without crashing', () => {
+    it('tests that the component does not crash when it is rendered', () => {
+      const helpers = renderWithRouterAndProviders(
+        <StudentDashboard history={{ location: { pathname: '/' } }} />
+      );
+    });
+  });
+
+  describe('should match the previous snapshot of the component', () => {
+    it('tests that the component has not changed and matched the previous snapshot', () => {
+      const tree = rendererWithRouterAndProviders(
+        <StudentDashboard history={{ location: { pathname: '/' } }} />
+      );
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+  });
+});
