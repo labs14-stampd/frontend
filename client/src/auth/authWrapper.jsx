7687c757bc4b274099442325b1413b533104de48
cecil john tantay <cjbtantay@gmail.com>
2019-08-22 10:28:01 -0700

    add token to local storage and to auth headers

--- client/src/auth/authWrapper.jsx
+++ client/src/auth/authWrapper.jsx
@@ -54,7 +54,7 @@ export const Auth0Provider = ({
 
         try {
           const result = await queries.register({ authToken });
-          localStorage.token = result.token;
+          localStorage.token = result.data.addUser.token;
           dispatchGlobal({ type: REGISTER, payload: result.data.addUser });
 
           if (result.data.addUser.roleId === null) {
--- client/src/clientQuery.js
+++ client/src/clientQuery.js
@@ -1,7 +1,41 @@
-import ApolloClient, { gql as graphql } from 'apollo-boost';
+// import ApolloClient, { gql as graphql, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost';
+
+// const httpLink = new HttpLink({uri: `${process.env.REACT_APP_GRAPHQL_URI}`})
+
+// const authLink = new
+
+// const client = new ApolloClient({
+//   uri:
+// });
+// export const gql = graphql;
+
+// export default client;
+
+import {
+  ApolloClient,
+  gql as graphql,
+  ApolloLink,
+  InMemoryCache,
+  HttpLink
+} from 'apollo-boost';
+
+const httpLink = new HttpLink({ uri: `${process.env.REACT_APP_GRAPHQL_URI}` });
+
+const authLink = new ApolloLink((operation, forward) => {
+  const { token } = localStorage;
+  // set HTTP headers
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  });
+  // Call next link to middleware chain
+  return forward(operation);
+});
 
 const client = new ApolloClient({
-  uri: `${process.env.REACT_APP_GRAPHQL_URI}`
+  link: authLink.concat(httpLink), // this chains it to HttpLink
+  cache: new InMemoryCache()
 });
 export const gql = graphql;
 
