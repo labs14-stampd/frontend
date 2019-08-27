587f5840293e7d2a44fcf84573159c3fbc5e6046
cecil john tantay <cjbtantay@gmail.com>
2019-08-26 17:03:44 -0700

    Add information to single view page

--- client/src/components/CredentialView/index.jsx
+++ client/src/components/CredentialView/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import jwt from 'jsonwebtoken';
+import { Anchor, Stack } from 'grommet';
 
 import queries from './queries';
 import emblem from '../../images/certEmblem.png';
@@ -30,8 +31,16 @@ const CredentialView = ({ match }) => {
     <div style={{ marginTop: '100px' }}>
       {credential ? (
         <CertificateArea>
-          <h1>Transaction hash: {credential.txHash}</h1>
-          <section>
+          <TopSection>
+            <h3>
+              Verified credential for 
+              {credential.ownerName}
+            </h3>
+              <p>verify transaction hash <span className="short"> {credential.txHash.slice(0, 6)}... </span> to the Ethereum network using <Anchor a11yTitle="Etherscan verification" target="_blank" color="brand" label="Etherscan" href={`https://rinkeby.etherscan.io/tx/${credential.txHash}`}>Etherscan</Anchor></p>    
+            <p>Status: {credential.valid ? (<Valid>Valid</Valid>) : (<NotValid>Not Valid</NotValid>)}</p>
+            <p>Expiration date: {credential.expirationDate || "none"}</p>
+          </TopSection>
+          <BottomSection>
             <div>
               <img src={credential.imageUrl || emblem} alt="school seal" />
             </div>
@@ -46,7 +55,7 @@ const CredentialView = ({ match }) => {
               {credential.name}
             </h3>
             <h2>{credential.ownerName || 'John Doe'}</h2>
-          </section>
+          </BottomSection>
           {/* DO NOT DELETE - ghost div for alignment */}
           <div />
         </CertificateArea>
@@ -73,41 +82,79 @@ const CredentialView = ({ match }) => {
 //   })
 // };
 
+const OurHouse = styled(Stack)`
+  .long {
+    display: none;
+    color: white;
+    background-color: black;
+  }
+  .short {
+    z-index: 99;
+    color: red;
+    &:hover {
+     color: green;
+    }
+  }
+`;
+
+const Valid = styled.span`
+  color: green;
+  font-size: 1.6rem;
+  font-weight: 700;
+`;
+const NotValid = styled.span`
+  color: red;
+  font-size: 1.6rem;
+  font-weight: 700;
+`;
+
 const CertificateArea = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   width: 100%;
+`;
 
-  section {
-    width: calc(100% - 500px);
-    max-width: 1000px;
-    background: ${props => props.theme.global.colors.dashBoardBg};
-    border: 1px solid ${props => props.theme.global.colors.dashBoardBorder};
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 37.5px 50px 32.5px;
-    margin: 0 auto;
+const BottomSection = styled.section`
+  width: calc(100% - 500px);
+  max-width: 1000px;
+  background: ${props => props.theme.global.colors.dashBoardBg};
+  border: 1px solid ${props => props.theme.global.colors.dashBoardBorder};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 37.5px 50px 32.5px;
+  margin: 0 auto;
 
-    & > * {
-      text-align: center;
-      margin-bottom: 15px;
-    }
+  & > * {
+    text-align: center;
+    margin-bottom: 15px;
+  }
 
-    & > *:first-child {
-      max-width: 125px;
-      margin-bottom: 50px;
-    }
+  & > *:first-child {
+    max-width: 125px;
+    margin-bottom: 50px;
+  }
 
-    & > *:last-child {
-      margin-top: 25px;
-    }
+  & > *:last-child {
+    margin-top: 25px;
   }
 
   div {
     width: 375px;
   }
+`;
+
+const TopSection = styled.section`
+  width: calc(100% - 500px);
+  max-width: 1000px;
+  background: ${props => props.theme.global.colors.dashBoardBg};
+  border: 1px solid ${props => props.theme.global.colors.dashBoardBorder};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 37.5px 50px 32.5px;
+  margin: 0 auto;
 `;
 
 export default CredentialView;
